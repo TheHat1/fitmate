dfa34347f8ad61a3d3fba5c32b2cfb4054a4d1ce
TheHat1 <robertoplamenov@abv.bg>
2026-07-02 11:48:20 +0300

    Add profile page. Add home page. Add font to css. Install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-
+import { useState } from "react"
+import Home from "./Pages/Home"
+import ProfilePage from "./Pages/ProfilePage"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
+  const [navigateTo, setNavigateTo] = useState('/login')
 
   return (
     <>
-      <div className='w-full h-23 bg-stone-700 border-b-8 border-amber-600'>
+      <div className='w-full h-23 bg-stone-700 border-b-8 border-amber-600 shadow-2xl flex justify-between p-2.5 items-center'>
+        <img/>
+        <div className="h-full flex items-center pr-3">
+          <img className="w-10 invert cursor-pointer transition-all duration-150 hover:scale-110" src='/Icons/user.png'/>
+        </div>
+      </div>
 
+      <div className="w-screen h-[calc(100vh-92px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<ProfilePage/>} />
+        </Routes>
+
+        <footer className="w-screen h-23 bg-stone-700 border-amber-600 border-t-8 shadow-2xl p-8 flex justify-between text-white unbounded">
+          <div className="overflow-y-hidden">
+            map
+          </div>
+          <div className="overflow-hidden">
+            ©FitMate
+          </div>
+        </footer>
       </div>
     </>
   )
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,29 @@
+import { useRef } from "react"
+
+
+export default function Home() {
+    const aboutRef = useRef()
+
+    return (
+        <>
+            <section className="w-screen h-[calc(100vh-92px)] flex justify-center items-center bg-black flex-col space-y-1">
+                <h1 className="text-amber-600 text-5xl unbounded h-15 font-bold">
+                    FitMate
+                </h1>
+                <h2 className="text-white text-2xl unbounded">
+                    лозонг лозонг лозонг
+                </h2>
+                <button onClick={() => {
+                    aboutRef.current.scrollIntoView({
+                        behavior: 'smooth'
+                    })
+                }}
+                    className="w-25 h-10 unbounded text-white bg-amber-600 mt-5 rounded-2xl treansition-all duration-200 hover:-translate-y-1 hover:bg-amber-400 cursor-pointer hover:scale-110">За нас</button>
+            </section>
+            <section ref={aboutRef} className="w-screen h-1/2 text-3xl p-15 unbounded flex flex-col justify-baseline">
+                <h1 className="overflow-x-hidden h-15">Какво е <a className="text-amber-600 overflow-hidden font-bold">FitMate</a>?</h1>
+                <p className="text-xl font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum harum aperiam obcaecati quibusdam voluptatum neque, dolores blanditiis quas tenetur, nesciunt consectetur? Impedit iure sed itaque. Laborum labore modi nihil? Nemo.</p>
+            </section>
+        </>
+    )
+}
