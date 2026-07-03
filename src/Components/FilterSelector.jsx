05be297cbe66be55e401b38a79129f722a3d202e
TheHat1 <robertoplamenov@abv.bg>
2026-07-03 12:52:47 +0300

    new filter toggle logic

--- src/Components/FilterSelector.jsx
+++ src/Components/FilterSelector.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 
-export default function FilterSelector({setFilterBy}) {
+export default function FilterSelector({ setFilterBy }) {
     const [footbalToggle, setFootballToggle] = useState(false)
     const [basketballToggle, setBasketballToggle] = useState(false)
     const [miscToggle, setMiscToggle] = useState(false)
@@ -24,35 +24,70 @@ export default function FilterSelector({setFilterBy}) {
         setFilterBy(filters)
     }
 
-    useEffect(()=>{
+    let active = 0
+
+    function handleFilterToggle(type) {
+        switch (type) {
+            case "football": {
+                if (footbalToggle) {
+                    active--
+                    setFootballToggle(false)
+                } else {
+                    active++
+                    setFootballToggle(true)
+                }
+                setToggleAll(false)
+            } break
+            case "basketball": {
+                if (basketballToggle) {
+                    active--
+                    setBasketballToggle(false)
+                } else {
+                    active++
+                    setBasketballToggle(true)
+                }
+                setToggleAll(false)
+            } break
+            case "misc": {
+                if (miscToggle) {
+                    active--
+                    setMiscToggle(false)
+                } else {
+                    active++
+                    setMiscToggle(true)
+                }
+                setToggleAll(false)
+            } break
+            default: {
+                setToggleAll(true)
+                setFootballToggle(false)
+                setBasketballToggle(false)
+                setMiscToggle(false)
+            }
+        }
+        if (active < 0) {
+            setToggleAll(true)
+            setFootballToggle(false)
+            setBasketballToggle(false)
+            setMiscToggle(false)
+        }
+    }
+
+    useEffect(() => {
         setFilter()
-    },[footbalToggle, basketballToggle, miscToggle, toggleAll])
+    }, [footbalToggle, basketballToggle, miscToggle, toggleAll])
 
     return (
         <>
             <div className="w-full flex flex-wrap h-fit min-h-10 items-center justify-center">
-                <button onClick={()=>{
-                    setToggleAll(true)
-                    setFootballToggle(false)
-                    setBasketballToggle(false)
-                    setMiscToggle(false)
-                }} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
-                    ${toggleAll ? "bg-amber-600 hover:bg-amber-500":"bg-gray-600 hover:bg-gray-500"}`}>Всичко</button>
-                                    <button onClick={()=>{
-                    setToggleAll(false)
-                    setFootballToggle(true)
-                }} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
-                    ${footbalToggle ? "bg-amber-600 hover:bg-amber-500":"bg-gray-600 hover:bg-gray-500"}`}>Футбол</button>
-                                    <button onClick={()=>{
-                    setToggleAll(false)
-                    setBasketballToggle(true)
-                }} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
-                    ${basketballToggle ? "bg-amber-600 hover:bg-amber-500":"bg-gray-600 hover:bg-gray-500"}`}>Баскетбол</button>
-                                    <button onClick={()=>{
-                    setToggleAll(false)
-                    setMiscToggle(true)
-                }} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
-                    ${miscToggle ? "bg-amber-600 hover:bg-amber-500":"bg-gray-600 hover:bg-gray-500"}`}>Други</button>
+                <button onClick={()=>{handleFilterToggle("active")}} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
+                    ${toggleAll ? "bg-amber-600 hover:bg-amber-500" : "bg-gray-600 hover:bg-gray-500"}`}>Всичко</button>
+                <button onClick={()=>{handleFilterToggle("football")}} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
+                    ${footbalToggle ? "bg-amber-600 hover:bg-amber-500" : "bg-gray-600 hover:bg-gray-500"}`}>Футбол</button>
+                <button onClick={()=>{handleFilterToggle("basketball")}} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
+                    ${basketballToggle ? "bg-amber-600 hover:bg-amber-500" : "bg-gray-600 hover:bg-gray-500"}`}>Баскетбол</button>
+                <button onClick={()=>{handleFilterToggle("misc")}} className={` text-white h-7 unbounded w-60 rounded-lg cursor-pointer  transition-all duration-150 hover:-translate-y-1 m-1
+                    ${miscToggle ? "bg-amber-600 hover:bg-amber-500" : "bg-gray-600 hover:bg-gray-500"}`}>Други</button>
             </div>
         </>
     )
