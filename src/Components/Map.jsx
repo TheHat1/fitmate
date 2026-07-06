36fa1ae9650424241c41726fc4d25d4fb5c5e30e
TheHat1 <robertoplamenov@abv.bg>
2026-07-06 09:15:40 +0300

    Set map tile provider

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -48,8 +48,8 @@ export default function Map({setActivityCords, isAddActivity}) {
             minZoom={13}
         >
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a>'
+                url={`https://api.maptiler.com/maps/outdoor-v4-dark/{z}/{x}/{y}.png?key=${import.meta.env.VITE_MAPTILER_API_KEY}`}
             />
             {addActivityHereMarkerCords === undefined ? null :
                 <Marker position={addActivityHereMarkerCords} icon={addActivityHereMarkerIcon}>
